cc068332f71fc77d4ae3d4c758b09026fd621cb6
Steven Lim <limstevenlbw@gmail.com>
2022-05-21 21:13:07 -0700

    Procedural Grid Mesh Test

--- Scripts/Models/Grid.cs
+++ Scripts/Models/Grid.cs
@@ -18,6 +18,7 @@ public class Grid : MonoBehaviour
     {
         WaitForSeconds wait = new WaitForSeconds(0.05f);
         vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        Vector2[] uv = new Vector2[vertices.Length];
 
         GetComponent<MeshFilter>().mesh = mesh = new Mesh();
         mesh.name = "Procedural Grid";
@@ -28,9 +29,12 @@ public class Grid : MonoBehaviour
             for (int x = 0; x <= xSize; x++, i++)
             {
                 vertices[i] = new Vector3(x, y);
+                uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
             }
         }
+
         mesh.vertices = vertices;
+        mesh.uv = uv;
 
         int[] triangles = new int[xSize * ySize * 6];
         for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
@@ -41,16 +45,18 @@ public class Grid : MonoBehaviour
                 triangles[ti + 3] = triangles[ti + 2] = vi + 1;
                 triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
                 triangles[ti + 5] = vi + xSize + 2;
+
+                mesh.triangles = triangles;
+                mesh.RecalculateNormals();
+
+                yield return wait;
             }
         }
-
-        mesh.triangles = triangles;
-        yield return wait;
+       
     }
 
     private void OnDrawGizmos()
     {
-
         if (vertices == null)
         {
             return;
